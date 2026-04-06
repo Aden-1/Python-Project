b2ad965b4898b31c2b8843c5619c572d9df803ff
Aden <adenthompson01@gmail.com>
2026-04-06 09:00:35 -0400

    Add files via upload

---
+++ quiz/project.py
@@ -0,0 +1,27 @@
+def main():
+    playing = 'y'
+    while playing != 'n':
+        userFirst, userLast, userID = getUserInfo()
+        getQuestion()
+        validateAnswer()
+        storeResults()
+        status = input("type 'Q' to quit and 'S' to restart the quiz.")
+        if status.lower() == 'q':
+            playing = 'n'
+        elif status.lower() == 's'
+            #restart quiz
+
+
+def getUserInfo():
+    validateInfo()
+
+def validateInfo():
+
+def getQuestion():
+
+def validateAnswer():
+
+def storeResults():
+
+if __name__ = __main__:
+    main()
