64418a84457797bde4b1784c1bdb8d6dd4a0e9f8
Komchen <nadiarae04@gmail.com>
2026-04-19 17:32:46 -0400

    memorypuzzle.py changes to boardwidth, board height and revealspeed

--- Memory Puzzle/memorypuzzle.py
+++ Memory Puzzle/memorypuzzle.py
@@ -9,11 +9,11 @@ from pygame.locals import *
 FPS = 30 # frames per second, the general speed of the program
 WINDOWWIDTH = 640 # size of window's width in pixels
 WINDOWHEIGHT = 480 # size of windows' height in pixels
-REVEALSPEED = 8 # speed boxes' sliding reveals and covers
+REVEALSPEED = 2 # speed boxes' sliding reveals and covers #NAD CHANGE TO MAKE SLOWER
 BOXSIZE = 40 # size of box height & width in pixels
 GAPSIZE = 10 # size of gap between boxes in pixels
-BOARDWIDTH = 10 # number of columns of icons
-BOARDHEIGHT = 7 # number of rows of icons
+BOARDWIDTH = 6 # number of columns of icons #NAD CHANGE TO SMALLER SIZE
+BOARDHEIGHT = 6 # number of rows of icons #NAD CHANGE TO SMALLER (HAS TO BE EVEN)
 assert (BOARDWIDTH * BOARDHEIGHT) % 2 == 0, 'Board needs to have an even number of boxes for pairs of matches.'
 XMARGIN = int((WINDOWWIDTH - (BOARDWIDTH * (BOXSIZE + GAPSIZE))) / 2)
 YMARGIN = int((WINDOWHEIGHT - (BOARDHEIGHT * (BOXSIZE + GAPSIZE))) / 2)
