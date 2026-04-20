c778bd0a6110071d533c5577de20f545e4f4793b
adent <adenthompson01@gmail.com>
2026-04-20 19:45:14 -0400

    Reverted size change

--- Memory Puzzle/memorypuzzle.py
+++ Memory Puzzle/memorypuzzle.py
@@ -12,8 +12,8 @@ WINDOWHEIGHT = 480 # size of windows' height in pixels
 REVEALSPEED = 2 # speed boxes' sliding reveals and covers #NAD CHANGE TO MAKE SLOWER
 BOXSIZE = 40 # size of box height & width in pixels
 GAPSIZE = 10 # size of gap between boxes in pixels
-BOARDWIDTH = 4 # number of columns of icons #NAD CHANGE TO SMALLER SIZE
-BOARDHEIGHT = 4 # number of rows of icons #NAD CHANGE TO SMALLER (HAS TO BE EVEN)
+BOARDWIDTH = 6 # number of columns of icons #NAD CHANGE TO SMALLER SIZE
+BOARDHEIGHT = 6 # number of rows of icons #NAD CHANGE TO SMALLER (HAS TO BE EVEN)
 assert (BOARDWIDTH * BOARDHEIGHT) % 2 == 0, 'Board needs to have an even number of boxes for pairs of matches.'
 XMARGIN = int((WINDOWWIDTH - (BOARDWIDTH * (BOXSIZE + GAPSIZE))) / 2)
 YMARGIN = int((WINDOWHEIGHT - (BOARDHEIGHT * (BOXSIZE + GAPSIZE))) / 2)
